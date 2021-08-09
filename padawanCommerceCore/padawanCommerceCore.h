bf35da40ea79f923f334292ac958d129957ef1a8
Diego Veronez Nunes <dveroneznunes@gmail.com>
2021-08-08 23:29:58 -0300

    Commit inicial

---
+++ padawanCommerceCore/padawanCommerceCore.h
@@ -0,0 +1,18 @@
+//
+//  padawanCommerceCore.h
+//  padawanCommerceCore
+//
+//  Created by Diego Veronez Nunes on 08/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for padawanCommerceCore.
+FOUNDATION_EXPORT double padawanCommerceCoreVersionNumber;
+
+//! Project version string for padawanCommerceCore.
+FOUNDATION_EXPORT const unsigned char padawanCommerceCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <padawanCommerceCore/PublicHeader.h>
+
+
---
+++ padawanCommerceCoreTests/CartTests.swift
@@ -0,0 +1,35 @@
+//
+//  CartTests.swift
+//  padawanCommerceCoreTests
+//
+//  Created by Diego Veronez Nunes on 08/08/21.
+//
+
+import XCTest
+
+class CartTests: XCTestCase {
+
+    override class func setUp() {
+        print("Setup da classe")
+    }
+    
+    override class func tearDown() {
+        print("Teardown da classe")
+    }
+    
+    override func setUpWithError() throws {
+        print("setUp da instancia")
+    }
+
+    override func tearDownWithError() throws {
+        print("tearDown da instancia")
+    }
+
+    func testOne() {
+        print("Meu teste 001")
+    }
+    
+    func testTwo() {
+        print("Meu teste 002")
+    }
+}
